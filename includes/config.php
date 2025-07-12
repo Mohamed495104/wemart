e1195a14b2dae4c4c72d72bb20985983dce6c392
Mohamed Ijas <71814248+Mohamed495104@users.noreply.github.com>
2025-07-12 14:11:03 -0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,8 +2,8 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'wemart');
-define('DB_USER', 'mdijas');
-define('DB_PASS', 'Ironman21');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('UPLOAD_DIR', 'assets/images/products/');
 define('BASE_URL', 'http://localhost/wemart/');
 define('API_KEY', 'YOUR_OPENWEATHERMAP_API_KEY'); // Replace with actual API key
